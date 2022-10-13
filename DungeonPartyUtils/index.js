f79bad28b232ffcd93854c0cd2044a2d8eb3dfef
Asumji <64544378+Asumji@users.noreply.github.com>
2022-10-13 20:51:37 +0200

    custom api key, armor, commands

--- DungeonPartyUtils/index.js
+++ DungeonPartyUtils/index.js
@@ -1,5 +1,12 @@
 import request from "requestV2/index";
-relevantItems = ["scylla", "hyperion", "astraea", "valkyrie", "terminator", "juju", "axe of the shredded", "livid dagger", "spirit bow", "last breath"]
+import PogObject from "PogData";
+
+const data = new PogObject("DungeonPartyUtils", {
+  apiKey: "",
+  relevantItems: ["scylla", "hyperion", "astraea", "valkyrie", "terminator", "juju", "axe of the shredded", "livid dagger", "spirit bow", "last breath"],
+});
+data.save();
+const chatPrefix = "§2[DPU]§r"
 
 const getrequest = function(url) {
   return request({
@@ -11,9 +18,28 @@ const getrequest = function(url) {
   });
 }
 
-function buildOutput(player, items) {
-  string = "§cName:§b " + player + "\n\n§l§6Items:§r\n" + items
-  ChatLib.chat(string)
+function decodeInv(data) {
+  let bytearray = java.util.Base64.getDecoder().decode(data);
+  let inputstream = new java.io.ByteArrayInputStream(bytearray);   
+  let nbt = net.minecraft.nbt.CompressedStreamTools.func_74796_a(inputstream); //CompressedStreamTools.readCompressed()                            
+  let items = nbt.func_150295_c("i", 10); //NBTTagCompound.getTagList()
+  
+  return items
+}
+
+function buildOutput(player, items, armor, secrets) {
+  var output = new Message(
+    "§cName:§b " + player + "\n\n§l§6Items:§r\n" + items + "\n§l§6Armor:§r\n" + armor + "\n§l§6Secrets: §c" + secrets,
+     new TextComponent("\n§4[Kick from Party]").setClick("run_command", "/party kick " + player));
+  ChatLib.chat(output)
+}
+
+function isDungeonItem(item) {
+  Lore = item.func_74781_a("tag").func_74781_a("display").func_74781_a("Lore")
+  if (Lore.toString().toLowerCase().includes("dungeon")) {
+    return true
+  }
+  return false
 }
 
 register('Chat', (event) => {
@@ -29,21 +55,18 @@ register('Chat', (event) => {
 
   getrequest("https://api.mojang.com/users/profiles/minecraft/" + name).then(response => {
     let uuid = response["id"];
-    getrequest("https://api.hypixel.net/player?key=" + "api-key" + "&uuid=" + uuid).then(response => {
-      getrequest("https://api.hypixel.net/skyblock/profiles?key=" + "dc30eb03-f088-4038-81a1-58e249f72c5c" + "&uuid=" + uuid).then(response => {
+    getrequest("https://api.hypixel.net/player?key=" + data.apiKey + "&uuid=" + uuid).then(response => {
+      let secrets = response["player"]["achievements"]["skyblock_treasure_hunter"]
+      getrequest("https://api.hypixel.net/skyblock/profiles?key=" + data.apiKey + "&uuid=" + uuid).then(response => {
         let profiles = response["profiles"]
         let itemString = ""
+        let armorString = ""
         profiles.forEach(profile => {
-          if (profile["members"][uuid]["inv_contents"] == null) {
-            itemString = itemString
-          } else {
-            let invContents = profile["members"][uuid]["inv_contents"]["data"]
-            let bytearray = java.util.Base64.getDecoder().decode(invContents);
-            let inputstream = new java.io.ByteArrayInputStream(bytearray);                                
-            let nbt = net.minecraft.nbt.CompressedStreamTools.func_74796_a(inputstream); //CompressedStreamTools.readCompressed()                            
-            let items = nbt.func_150295_c("i", 10); //NBTTagCompound.getTagList()
-            let length = items.func_74745_c(); //NBTTagList.tagCount()
+          if (profile["members"][uuid]["inv_contents"] != null) {
+            // Build Item String
 
+            let items = decodeInv(profile["members"][uuid]["inv_contents"]["data"])
+            let length = items.func_74745_c(); //NBTTagList.tagCount()
             itemArray = []
             for(let i = 0; i < length; i++){                                    
               item = items.func_150305_b(i); //NBTTagList.getCompoundTagAt()
@@ -54,26 +77,105 @@ register('Chat', (event) => {
             }
             string = ""
             for(let i = 0; i < itemArray.length; i++) {
-              for (let j = 0; j < relevantItems.length; j++) {
-                if (itemArray[i].toLowerCase().includes(relevantItems[j])) {
+              for (let j = 0; j < data.relevantItems.length; j++) {
+                if (itemArray[i].toLowerCase().includes(data.relevantItems[j])) {
                   string = string + " " + itemArray[i]
                 }
               }
             }
             itemString = itemString + profile["cute_name"] + ":" + string + "§r\n"
           }
+          if (profile["members"][uuid]["inv_armor"] != null) {
+            //Build Armor String
+
+            let armor = decodeInv(profile["members"][uuid]["inv_armor"]["data"])
+            let length2 = armor.func_74745_c();
+            string = ""
+            for (let i = length2; i > -1; i--) {
+              armorPiece = armor.func_150305_b(i)
+              if(!armorPiece.func_82582_d()) {
+                if (isDungeonItem(armorPiece)) {
+                  Name = armorPiece.func_74781_a("tag").func_74781_a("display").func_74781_a("Name").toString().replace(/"/g,"")
+                  string = string + " " + Name
+                }
+              }
+            }
+            if (string != "") {
+              armorString = armorString + profile["cute_name"] + ":" + string + "§r\n"
+            }
+          }
         })
-        buildOutput(name, itemString)
+        buildOutput(name, itemString, armorString, secrets)
       })
     })
   });
-
 }).setChatCriteria("joined the dungeon group!").setContains();
 
+register("command", (...args) => {
+  const helpMessage = "§6Help\n§a/dpu key <API key>\n§2Set your api key.\n§a/dpu add <item>\n§2Add an Item to check for.\n§a/dpu remove <item>\n§2Remove an item that is being checked for.\n§a/dpu list\n§2List all items the mod currently checks for."
+  if (args) {
+    if (args[0] == "key") {
+      if (args[1]) {
+        getrequest("https://api.hypixel.net/player?key="+args[1]+"&uuid=6abd94a3f33940dd95e49eaa46ee8016").then(response => {
+          data.apiKey = args[1]
+          data.save()
+          ChatLib.chat("§aSuccessfully set the API key!")
+        }).catch(error => {
+          if (error["cause"] == "Invalid API key") {
+            ChatLib.chat("§cAPI Key is invalid!")
+          }
+        })
+      } else {
+        ChatLib.chat("§cDidn't provide an API key!")
+      }
+    } else if (args[0] == "add") {
+      if (args[1]) {
+        if (!data.relevantItems.includes(args.slice(1).join(" ").toLowerCase())) {
+          data.relevantItems.push(args.slice(1).join(" ").toLowerCase())
+          data.save()
+          ChatLib.chat("§aThe mod now checks for " + args.slice(1).join(" ") + ".")
+        } else {
+          ChatLib.chat("§cItem is already being checked.")
+        }
+      } else {
+        ChatLib.chat("§cDidn't provide an item.")
+      }
+    } else if (args[0] == "remove") {
+      if (args[1]) {
+        if (data.relevantItems.includes(args.slice(1).join(" ").toLowerCase())) {
+          data.relevantItems.splice(data.relevantItems.indexOf(args.slice(1).join(" ").toLowerCase()),1)
+          data.save()
+          ChatLib.chat("§aThe mod no longer checks for " + args.slice(1).join(" ") + ".")
+        } else {
+          ChatLib.chat("§cItem isn't being checked.")
+        }
+      } else {
+        ChatLib.chat("§cDidn't provide an item.")
+      }
+    } else if (args[0] == "list") {
+      string = "§2Mod currently checks for:\n"
+      for (let i = 0; i < data.relevantItems.length; i++) {
+        if (i != data.relevantItems.length - 1) {
+          string = string + "§a" + data.relevantItems[i] + ", "
+        } else {
+          string = string + "§a" + data.relevantItems[i]
+        }
+      }
+      ChatLib.chat(string)
+    } else {
+      ChatLib.chat(helpMessage)
+    }
+  } else {
+    ChatLib.chat(helpMessage)
+  }
+}).setName("dpu")
 
-
-
-
-
-
-//{id:267s,Count:1b,tag:{ench:[],Unbreakable:1b,HideFlags:254,display:{Lore:[0:"§7Gear Score: §d966 §8(3208)",1:"§7Damage: §c+354 §e(+20) §8(+1,370.13)",2:"§7Strength: §c+185 §e(+20) §8(+712.3)",3:"§7Crit Chance: §c+23.2% §9(+10%) §8(+33%)",4:"§7Crit Damage: §c+235.5% §9(+110%) §8(+972.08%)",5:"§7Intelligence: §a+55 §8(+209.5)",6:"§7Ferocity: §a+33 §8(+45)",7:" §8[§8⚔§8] §8[§8⚔§8]",8:"",9:"§d§l§d§lUltimate Wise V§9, §9Champion X§9, §9Cleave V",10:"§9Critical V§9, §9Cubism V§9, §9Ender Slayer V",11:"§9Experience III§9, §9Fire Aspect II§9, §9First Strike IV",12:"§9Giant Killer V§9, §9Impaling III§9, §9Lethality V",13:"§9Looting III§9, §9Luck V§9, §9Prosecute V",14:"§9Scavenger III§9, §9Smite VII§9, §9Syphon III",15:"§9Thunderlord V§9, §9Vampirism V§9, §9Venomous V",16:"",17:"§a◆ Jerry Rune III",18:"",19:"§7Deals +§c50% §7damage to",20:"§7Withers. Grants §c+1 §c❁ Damage",21:"§c§7and §a+1 §9☠ Crit Damage §7per",22:"§7§cCatacombs §7level.",23:"",24:"§aScroll Abilities:",25:"§6Ability: Wither Impact §e§lRIGHT CLICK",26:"§7Teleport §a10 blocks§7 ahead of",27:"§7you. Then implode dealing",28:"§7§c19,435 §7damage to nearby",29:"§7enemies. Also applies the wither",30:"§7shield scroll ability reducing",31:"§7damage taken and granting an",32:"§7absorption shield for §e5",33:"§e§7seconds.",34:"§8Mana Cost: §3150",35:"",36:"§9Suspicious Bonus",37:"§7Increases weapon damage by",38:"§7§c+15§7.",39:"",40:"§d§l§ka§r §d§l§d§lMYTHIC DUNGEON SWORD §d§l§ka"],Name:"§dSuspicious Scylla §6✪§6✪§6✪§6✪§6✪"},ExtraAttributes:{rarity_upgrades:1,hot_potato_count:10,runes:{JERRY:3},champion_combat_xp:7321829.6145212995d,modifier:"suspicious",upgrade_level:5,id:"SCYLLA",enchantments:{impaling:3,luck:5,critical:5,cleave:5,smite:7,looting:3,syphon:3,scavenger:3,ender_slayer:5,fire_aspect:2,experience:3,vampirism:5,giant_killer:5,first_strike:4,venomous:5,thunderlord:5,ultimate_wise:5,cubism:5,champion:10,lethality:5,PROSECUTE:5},uuid:"51325c5f-7b95-446c-92cc-a259f81c0380",ability_scroll:[0:"WITHER_SHIELD_SCROLL",1:"IMPLOSION_SCROLL",2:"SHADOW_WARP_SCROLL"],timestamp:"8/16/22 6:00 AM"}},Damage:0s}
+register('Chat', (event) => {
+  let unformattedMessage = ChatLib.removeFormatting(ChatLib.getChatMessage(event))
+  unformattedMessage = unformattedMessage.replace(/ /g, "").replace("YournewAPIkeyis", "")
+  getrequest("https://api.hypixel.net/player?key="+unformattedMessage+"&uuid=6abd94a3f33940dd95e49eaa46ee8016").then(response => {
+    ChatLib.chat(chatPrefix + " §aYour key has been set to §6" + unformattedMessage)
+    data.apiKey = unformattedMessage
+    data.save()
+  })
+}).setChatCriteria("Your new API key is ").setContains()
